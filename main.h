0b85f8142e3eeb2d19cea4e7bef14fc39ecfeeae
yonasTK <48440474+yonasTK@users.noreply.github.com>
2022-08-08 11:57:49 +0300

    Update main.h

--- main.h
+++ main.h
@@ -17,7 +17,7 @@ typedef struct print
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
-int print_s(va_list s);
+int print_S(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
 int print_f(va_list f);
