636304b419776370616f2663368e20023ce46498
fitunabu <106749942+fitunabu@users.noreply.github.com>
2022-08-08 18:01:28 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,17 +12,6 @@ int (*check_format(const char *format))(va_list)
 	print_t p[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"b", print_b},
-		{"u", print_u},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
 		{NULL, NULL}
 	};
 
