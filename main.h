f652cdbda3e275d1631b1eee375e56b2b3aa826b
fitunabu <106749942+fitunabu@users.noreply.github.com>
2022-08-09 21:35:06 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,26 +1,46 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdarg.h>
 
 /**
-* struct print - structure for printing various types
-* @t: type to print
-* @f: function to print
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
 */
-typedef struct print
+struct convert
 {
-	char *t;
+	char *sym;
 	int (*f)(va_list);
-} print_t;
+};
+typedef struct convert conver_t;
 
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_d(va_list d);
-int print_i(va_list i);
-int print_x(va_list x);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
-#endif  /* _MAIN_H */
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
+
+#endif
