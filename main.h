56a2b1ad28b5365b0bde618a026640b93187be2d
fitunabu <106749942+fitunabu@users.noreply.github.com>
2022-08-09 10:01:57 +0300

    Update main.h

--- main.h
+++ main.h
@@ -19,6 +19,8 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
 int print_x(va_list x);
 
 #endif  /* _MAIN_H */
